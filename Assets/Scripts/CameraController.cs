c0985efce14e0db0837919ffaa9cbd3bd9f1b1b1
excaliber90 <thapaprashant06@gmail.com>
2025-07-30 22:50:43 +0545

    Adding buttons and controlling the game object

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+     //Camel case notation: prashantThapa
+    public Camera _camera;
+    public float zoomSize= 0.01f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraSizeZoomInZoomOut();
+    }
+    void CameraSizeZoomInZoomOut(){
+        if (Input.GetKey(KeyCode.Q)){
+            if(_camera.orthographicSize <10){
+                _camera.orthographicSize += zoomSize;
+                }
+        }
+         if (Input.GetKey(KeyCode.E)){
+            if (_camera.orthographicSize>1){
+                _camera.orthographicSize -= zoomSize;
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Movement : MonoBehaviour
+{
+
+    public float xSpeed = 0.5f;
+    public float ySpeed = 0.5f;
+    public float zSpeed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Move(); 
+       Rotate();
+    }
+    void Move()
+    {
+        if(Input.GetKey(KeyCode.D)){
+
+        //to change the position of the object
+                transform.position +=new Vector3(xSpeed,0,0);
+
+        }
+          if(Input.GetKey(KeyCode.A)){
+            MoveLeft();
+
+        }
+         if(Input.GetKey(KeyCode.W)){
+
+        //to change the position of the object
+        MoveUp();
+
+        }
+          if(Input.GetKey(KeyCode.S)){
+            MoveDown();
+
+        }
+         if(Input.GetKey(KeyCode.Z)){
+
+        //to change the position of the object
+                transform.position +=new Vector3(0,0,zSpeed);
+
+        }
+          if(Input.GetKey(KeyCode.X)){
+
+                transform.position -=new Vector3(0,0,zSpeed);
+
+        }
+    }
+   void Rotate(){
+    if (Input.GetKey(KeyCode.J))
+{
+    transform.Rotate(Vector3.right * xSpeed * Time.deltaTime); // Rotate around X axis
+}
+if (Input.GetKey(KeyCode.L))
+{
+    transform.Rotate(Vector3.left * xSpeed * Time.deltaTime); // Rotate around X axis
+}
+if (Input.GetKey(KeyCode.I))
+{
+    transform.Rotate(Vector3.up * ySpeed * Time.deltaTime); // Rotate around Y axis
+}
+if (Input.GetKey(KeyCode.K))
+{
+    transform.Rotate(Vector3.down * ySpeed * Time.deltaTime); // Rotate around Y axis
+}
+
+
+    }
+    public void MoveLeft(){
+
+            transform.position -=new Vector3(xSpeed,0,0);
+    }
+    public void MoveRight(){
+            transform.position +=new Vector3(xSpeed,0,0);
+    }
+    public void MoveUp(){
+        transform.position +=new Vector3(0,ySpeed,0);
+    }
+    public void MoveDown()
+    {
+    transform.position -=new Vector3(0,ySpeed,0);
+    }
+}
